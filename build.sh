6df9438f5986351c2f667c31d9caa8bcc51f0b74
modellers <115144400+modellers@users.noreply.github.com>
2023-02-10 09:21:33 +0000

    Build script prep

---
+++ build.sh
@@ -0,0 +1,5 @@
+export EXPO_IOS_DIST_P12_PASSWORD=
+turtle build:ios \\
+  --team-id 32NUMPL6TJ \\
+  --dist-p12-path /path/to/your/dist/cert.p12 \\
+  --provisioning-profile-path /path/to/your/provisioning/profile.mobileprovision
